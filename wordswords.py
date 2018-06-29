3f07bd3f7f4556da44c4cf142e8ae6f8295cf70b
Ana Fabela <ana.fabelah@gmail.com>
2018-06-29 16:18:34 +1000

    third commit, I want to find the line where the duplicate word is located in the original file and eventually erase the duplicate.

--- wordswords.py
+++ wordswords.py
@@ -5,22 +5,38 @@ input_file = open("test.txt", 'r')
 
 words = []
 duplicates = []
+line_list = []
 
-line_number = 1
 for line in input_file:
-    lines = line.split()
-    for word in lines:
+    line_list.append(line)
+    split_lines = line.split()
+
+    for word in split_lines:
         if word != "'\n'":
-            # print(words)
             words.append(word)
-print(words)
 
-for i in range(length(words)):
-    for index, word in enumerate(words):
-        print(index, word)
-        if words[index] == words[index + 1]:
-            duplicates.append([word, line_number])
-            previous_word = word
-        line_number += 1
+word_number = 1
+previous_word = None
 
-print(duplicates)        
+# I also want to find the line where the duplicate word is....
+
+for i in range(len(words)):
+    if i == len(words) - 1:
+        break      
+    if words[i] == previous_word:
+        # print("duplicate word found, word appended in duplicates list.")
+        duplicates.append([word_number, words[i]])
+        previous_word = words[i]
+
+    else:
+        previous_word = words[i]
+
+    word_number += 1
+
+# print(line_list)
+# print(words)
+# print('\n')
+print("Duplicate word list")
+print("[word number, word]")
+for j in duplicates:
+    print(j)
