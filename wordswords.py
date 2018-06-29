d68cfb206e1c960ecb3704a0f61f12a6f044f7ad
Ana Fabela <ana.fabelah@gmail.com>
2018-06-29 12:21:03 +1000

    second commit, managed to print the word list, but still battling with getting the duplicate list indexing correct.

--- wordswords.py
+++ wordswords.py
@@ -1,24 +1,26 @@
 # duplicate adjacent words?
 import string
 
-input_file = open(test.txt, 'r')
+input_file = open("test.txt", 'r')
 
 words = []
 duplicates = []
 
 line_number = 1
 for line in input_file:
-    lines = line_str.split()
-    previous_word = None
+    lines = line.split()
     for word in lines:
         if word != "'\n'":
+            # print(words)
             words.append(word)
-        if word == previous_word:
-            duplicates.append([word, line_number])
-        previous_word = word
-    line_number += 1
+print(words)
 
-for index, word in enumerate(words):
-    print(index, word)
-    if words[index] == words[index + 1]:
-        duplicates.append(word)
+for i in range(length(words)):
+    for index, word in enumerate(words):
+        print(index, word)
+        if words[index] == words[index + 1]:
+            duplicates.append([word, line_number])
+            previous_word = word
+        line_number += 1
+
+print(duplicates)        
