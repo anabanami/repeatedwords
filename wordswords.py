c15babac9ee50d8e3d81ac9317c1fb334e8fd75e
Ana Fabela <ana.fabelah@gmail.com>
2018-06-29 20:32:36 +1000

    line number works now.

--- wordswords.py
+++ wordswords.py
@@ -8,13 +8,16 @@ duplicates = []
 line_number_list = []
 
 for line_number, line in enumerate(input_file, 1):
-    line_number_list.append(line_number)
     split_lines = line.split()
 
     for word in split_lines:
         if word != "'\n'":
             lowercase_word = word.lower()
             words.append(lowercase_word)
+            line_number_list.append(line_number)
+
+# print(len(line_number_list))
+# print(len(words))
 
 word_number = 1
 previous_word = None
@@ -25,7 +28,7 @@ for i in range(len(words)):
     
     if words[i] == previous_word:
         # print("duplicate word found, word appended in 'duplicates'.")
-        duplicates.append([line_number, word_number, words[i]])
+        duplicates.append([line_number_list[i], word_number, words[i]])
         previous_word = words[i]
 
     else:
@@ -33,12 +36,7 @@ for i in range(len(words)):
 
     word_number += 1
 
-
-# print(words)
-# print('\n')
 print("Duplicate word list")
 print("[line, word number, word]")
 for k in duplicates:
     print(k)
-
-# I want to find the line where the duplicate word is....
